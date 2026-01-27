e2f9c152893aa5c68260fb7946f60bfd8f525c7e
Link4Electronics <link4electronics@gmail.com>
2026-01-27 05:55:03 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,11 +8,16 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/pixmaps/ghostship.png
+export DESKTOP=/usr/share/applications/ghostship.desktop
+export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun /usr/bin/Ghostship
+mv /opt/ghostship/ghostship.o2r ./AppDir/bin
+mv /opt/ghostship/config.yml ./AppDir/bin
+mv /opt/ghostship/gamecontrollerdb.txt ./AppDir/bin
+echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/bin' >> ./AppDir/.env
 
 # Additional changes can be done in between here
 

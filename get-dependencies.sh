172b0b9b7163ab8f5a15098bad869fad1f092ea5
Link4Electronics <link4electronics@gmail.com>
2026-04-07 04:53:28 -0300

    Update get-dependencies.sh and make-appimage.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -9,22 +9,20 @@ echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
     cmake         \
     fmt           \
-    libdecor      \
     libzip        \
-    ninja         \
+    mbedtls3      \
     nlohmann-json \
-    sdl2          \
+    sdl2-compat   \
     spdlog        \
+    tcc           \
     tinyxml2
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
-# Comment this out if you need an AUR package
 make-aur-package zenity-rs-bin
 
-# If the application needs to be manually built that has to be done down here
 echo "Making stable build of Ghostship..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/HarbourMasters/Ghostship"
@@ -34,17 +32,40 @@ echo "$VERSION" > ~/version
 
 mkdir -p ./AppDir/bin
 cd ./Ghostship
-patch -Np1 -i "../ghostship-fix-mtxf_copy-incorrect-values.patch"
+#patch -Np1 -i "../ghostship-fix-mtxf_copy-incorrect-values.patch"
+
+# On aarch64, GNU ld fails with ".eh_frame_hdr refers to overlapping FDEs"
+# because libtcc1.a is compiled by tcc itself and tcc's arm64 .eh_frame
+# generation is broken. Build it with the system compiler instead (official
+# tinycc knob) and skip .eh_frame_hdr generation as a safety net.
+LINKER_FLAGS=""
+if [ "$ARCH" = "aarch64" ]; then
+    sed -i 's|-C "[$][{]tinycc_SOURCE_DIR[}]/lib"|& arm64-libtcc1-usegcc=yes|' \
+        libultraship/cmake/dependencies/common.cmake
+    grep -q 'arm64-libtcc1-usegcc=yes' \
+        libultraship/cmake/dependencies/common.cmake
+    LINKER_FLAGS="-DCMAKE_EXE_LINKER_FLAGS=-Wl,--no-eh-frame-hdr"
+fi
+
 cmake . \
     -Bbuild \
-    -GNinja \
-    -DNON_PORTABLE=On
-cmake --build build --config Release
-cmake --build build --config Release --target GeneratePortO2R
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+    -DCMAKE_INCLUDE_PATH=/usr/include/mbedtls3 \
+    -DCMAKE_LIBRARY_PATH=/usr/lib/mbedtls3 \
+    $LINKER_FLAGS
 
-mv -v build/assets ../AppDir/bin
-mv -v build/Ghostship ../AppDir/bin
-mv -v build/config.yml ../AppDir/bin
-mv -v build/ghostship.o2r ../AppDir/bin
+# armflush.c calls __arm64_clear_cache(), a tcc-only builtin. With
+# arm64-libtcc1-usegcc=yes the file is compiled by GCC instead, where
+# that is an implicit-declaration error (GCC 14+). The tinycc sources
+# only exist after configure (FetchContent), so patch them here.
+if [ "$ARCH" = "aarch64" ]; then
+    sed -i 's|__arm64_clear_cache(beg, end);|__builtin___clear_cache(beg, end);|' \
+        build/_deps/tinycc-src/lib/armflush.c
+fi
+
+cmake --build build --config Release -j$(nproc)
+cmake --build build --config Release --target GeneratePortO2R -j$(nproc)
+
+mv -v build/assets build/Ghostship build/config.yml build/ghostship.o2r ../AppDir/bin
 wget -O ../AppDir/bin/gamecontrollerdb.txt https://raw.githubusercontent.com/mdqinc/SDL_GameControllerDB/master/gamecontrollerdb.txt
-cp -rv logo.png /usr/share/pixmaps/ghostship.png
--- make-appimage.sh
+++ make-appimage.sh
@@ -5,15 +5,20 @@ set -eu
 ARCH=$(uname -m)
 export ARCH
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/pixmaps/ghostship.png
+export ICON=https://raw.githubusercontent.com/HarbourMasters/Ghostship/refs/heads/develop/logo.png
+export DESKTOP=https://raw.githubusercontent.com/HarbourMasters/Ghostship/refs/heads/develop/Ghostship.desktop
+export STARTUPWMCLASS=Ghostship
 export DEPLOY_OPENGL=1
+export DEPLOY_VULKAN=1
 
 # Deploy dependencies
 quick-sharun ./AppDir/bin/* /usr/bin/zenity
 
-# Additional changes can be done in between here
-
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the app normally quits before that time
+# then skip this or check if some flag can be passed that makes it stay open
+quick-sharun --simple-test ./dist/*.AppImage
